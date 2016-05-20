f3d0b955ef2cc8d3160052120160ea2d6bd0149c
Henning Schneider <henning.w.schneider@gmail.com>
2016-05-20 10:30:04 +0200

    first ropund of Linting&Co

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,77 @@
-var gulp;
-gulp = require('gulp');
+var gulp    = require('gulp');
+var gulpTypeScript    = require('gulp-typescript');
+var gulpJade    = require('gulp-jade');
+var gulpPostCss = require('gulp-postcss');
 
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// JADE=>HTML
 gulp.task(
-	'default',
-	function() {
-		//place code for your default task here
-		console.log('test');
+	'jade',
+	function () {
+		var YOUR_LOCALS = {};
+
+		gulp.src('./src/jade/*.jade')
+			.pipe(gulpJade({locals: YOUR_LOCALS}))
+			.pipe(gulp.dest('./dist/'))
+			.pipe(reload({ stream:true }));
+	}
+);
+
+// PostCSS=>CSS
+gulp.task(
+	'css',
+	function(){
+		return gulp.src('./src/postcss/*.css')
+			.pipe(
+				gulpPostCss(
+					[
+						autoprefixer({browsers:['last 1 version']}),
+						cssnano()
+					]
+				)
+			)
+			.pipe(gulp.dest('./dist/css'))
+			.pipe(reload({ stream:true }));
+	}
+);
+
+// TYPESCRIPT=>JAVASCRIPT
+gulp.task(
+	'script',
+	function(){
+		"use strict";
+		return gulp.src('./src/typescript/*.ts')
+			.pipe(
+				gulpTypeScript(
+					{
+						noImplicitAny: true,
+						out: 'script.js'
+					}
+				)
+			)
+			.pipe((gulp.dest('./dist')))
+			.pipe(reload({ stream:true }));
+	}
+);
+
+// LIVE-RELOAD-SERVER
+// watch files for changes and reload
+gulp.task(
+	'serve',
+	['jade', 'css'],
+	function(){
+		browserSync(
+			{
+				server: {
+					baseDir:'dist'
+				}
+			}
+		);
+		gulp.watch('./src/postcss/*.css', ['css']);
+		gulp.watch('./src/jade/*.jade', ['jade']);
 	}
 );
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { bootstrap } from "angular2/platform-browser-dynamic";
+import { AppComponent } from "./app.component.ts";
+
+bootstrap(AppComponent);
